27cc80c496fdde8e66cba18b47067ddf75906e15
Patrick W <pat.realrealestatellc@gmail.com>
2020-08-20 01:55:50 -0400

    added font-family's from that I downloaded from Google.

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-$(document).ready(function(e) {
-        var width = $(document).width()
-    ;
-    
-    function goLeft() { 
-        $("animate").animate ({
-            left: 0
-        }, 5000, function() {
-            setTimeout(goRight, 50);
-        });
-    }
 
